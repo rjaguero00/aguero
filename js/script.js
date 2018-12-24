ff563ad1d2cdf5606f43d1d04e6ec338ad7fa1bf
Roberto aguero <rjaguero00@gmail.com>
2018-12-23 22:35:11 -0600

    heading/typedjs

--- js/script.js
+++ js/script.js
@@ -5,4 +5,12 @@ $(document).ready(function () {
         pagination: false,
         nav: '.slides-navigation',
     });
+
+    var typed = new Typed(".typed", {
+        strings: ["Front-End Developer.", "Web Design.", "Student."],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
 });
