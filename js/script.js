5b003c47b6991a94cf21544c30ed5fff079a7b43
Roberto aguero <rjaguero00@gmail.com>
2018-12-30 23:11:03 -0600

    loading icon

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+$(window).on("load", function () {
+
+    $(".loader .inner").fadeOut(750, function () {
+        $(".loader").fadeOut(1000);
+    });
+})
+
+
 $(document).ready(function () {
     $('#slides').superslides({
         animation: 'fade',
